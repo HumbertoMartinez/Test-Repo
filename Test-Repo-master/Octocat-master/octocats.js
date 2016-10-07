940cdb8cd4e473f4714af574b28bf9a6a5c92319
HumbertoMartinez <9hmartinez@stpatrick.org>
2016-10-07 12:54:07 -0500

    Octocat day 2

---
+++ Test-Repo-master/Octocat-master/octocats.js
@@ -0,0 +1,20 @@
+//Write your constructor function below.
+//You should have 2 instance variables and 1 instance function included.
+function Octocat(name,arms){
+
+this.name
+this.arms
+}
+this.slap = function(){
+  for(var a=0;a<this.arms;a++)
+}
+
+
+
+
+
+
+
+
+
+//Write your 3 new octocat objects below here.
